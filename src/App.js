bb9962ef0f48f4561476385b5092f59dabbe52ca
OSHIN <81525447+GitHubOshin@users.noreply.github.com>
2023-01-24 23:47:12 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,42 @@
+import React, { useState } from 'react'
 import './App.css'
 
+import Input from './Input'
+import Navbar from './Navbar'
+import Post from './Post'
+
+let id = 1
+
 function App() {
-  return <div className="App">REACT</div>
+  const [posts, setPosts] = useState([])
+
+  function addPost(title) {
+    // const newPost = { id: id, title: title }
+    const newPost = { id, title }
+    setPosts([newPost, ...posts])
+    id += 1
+    console.log(posts)
+  }
+
+  function deletePost(id) {
+    const updatedPosts = posts.filter((post) => post.id !== id)
+    setPosts(updatedPosts)
+  }
+
+  return (
+    <div className="App">
+      <Navbar />
+      <Input addPost={addPost} />
+      {posts.map((post) => (
+        <Post
+          key={post.id}
+          id={post.id}
+          title={post.title}
+          deletePost={deletePost}
+        />
+      ))}
+    </div>
+  )
 }
 
 export default App
